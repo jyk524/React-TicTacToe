d09e333e543297fe4cd1b32ebcdff9c79492dd42
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-21 16:48:01 -0500

    removed state that was pushed up

--- src/index.js
+++ src/index.js
@@ -20,13 +20,6 @@ class Board extends React.Component {
     );
   }
   render() {
-    const winner = calculateWinner(this.props.squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.props.xIsNext ? "X" : "O");
-    }
     return (
       <div>
         <div className="board-row">
